cae6eb2b31202d969e015f89c816f53c158aec8d
Exreller <pipkonp@mail.ru>
2022-05-01 09:56:10 +0300

    Использовал \n для переноса строки

--- 3.x/Program.cs
+++ 3.x/Program.cs
@@ -6,7 +6,7 @@ namespace _3.x
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет,\n мир!");
         }
     }
 }
